b5c29caa45cde4c53ee261c1bc6c2db5b757284e
yousef badr <yousefbadr10@gmail.com>
2024-03-02 04:44:36 +0200

    react lab edits

--- src/task2.jsx
+++ src/task2.jsx
@@ -5,11 +5,11 @@ class Task2 extends Component {
     super(props);
     this.state = {
       images: [
-        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flipkart.com%2Fcute-dogs-animated-hd-matte-finish-poster-paper-print%2Fp%2Fitm682192dd3afbe&psig=AOvVaw12T-SwXAXx847Rb10bdX9D&ust=1709310734178000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKiH9uz80IQDFQAAAAAdAAAAABAD',
-        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%2Fimages%3Fk%3Dpuppy%2Bcartoon&psig=AOvVaw12T-SwXAXx847Rb10bdX9D&ust=1709310734178000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKiH9uz80IQDFQAAAAAdAAAAABAQ',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDxKhPlg-tK801pmTlua-t7ui4qIm3OCDMoA&usqp=CAU',
-        'https://t3.ftcdn.net/jpg/00/50/07/64/360_F_50076454_TCvZEw37VyB5ZhcwEjkJHddtuV1cFmKY.jpg',
-        'https://t3.ftcdn.net/jpg/05/68/02/36/360_F_568023692_15idt2j4V5fvXr2YDTbijyl292IRyyMJ.jpg',
+        'https://cdn.dummyjson.com/product-images/1/1.jpg',
+        'https://cdn.dummyjson.com/product-images/2/1.jpg',
+        'https://cdn.dummyjson.com/product-images/3/1.jpg',
+        'https://cdn.dummyjson.com/product-images/5/1.jpg',
+        'https://cdn.dummyjson.com/product-images/4/1.jpg',
       ],
       currentIndex: 0,
       isPlaying: false,
